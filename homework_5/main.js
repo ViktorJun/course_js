30eba58d3ff71ff84e70028bd4ae0780892e8800
ViktorJun <viktorbylash@gmail.com>
2024-07-03 21:39:59 +0300

    homework_5.2

--- homework_5/main.js
+++ homework_5/main.js
@@ -1,6 +1,16 @@
 // homework_5.1
-let num = 19.5
-while (num !== 30) {
-    num += 0.5
-    console.log(num)
+// let num = 19.5
+// while (num !== 30) {
+//     num += 0.5
+//     console.log(num)
+// }
+
+// homework_5.2
+let dollar = 26
+let uah = prompt(`Введите суму в долларах (1 доллар = ${dollar} грн)`)
+if (!isNaN(Number(uah)) && uah !== null && uah.trim() !== '') {
+    let convert = dollar * uah
+    alert(`Результат конвертации: ${convert}`)
+} else {
+    alert(`Нужно ввести число`)
 }
