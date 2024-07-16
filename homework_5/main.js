73dc1b788074dad9f95753afcc032a7afe18329a
ViktorJun <viktorbylash@gmail.com>
2024-07-16 19:47:33 +0300

    homework_5.4

--- homework_5/main.js
+++ homework_5/main.js
@@ -16,17 +16,40 @@
 // }
 
 // homework_5.3
+// let number = prompt('Введите число')
+// let flag = true
+// let i = 0
+// while (flag) {
+//     i = ++i
+//     let sqrt = i * i
+//     if (Number.isInteger(sqrt)){
+//         if (sqrt <= number && i <= 100) {
+//             console.log(sqrt)
+//         } else {
+//             flag = false
+//         }
+//     }
+// }
+
+// homework_5.4
 let number = prompt('Введите число')
-let flag = true
-let i = 0
-while (flag) {
-    i = ++i
-    let sqrt = i * i
-    if (Number.isInteger(sqrt)){
-        if (sqrt <= number && i <= 100) {
-            console.log(sqrt)
-        } else {
-            flag = false
+number = parseInt(number);
+function isPrime(num) {
+    if (num <= 1) {
+        return false
+    }
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) {
+            return false
         }
     }
+    return true
 }
+if (isPrime(number)) {
+    console.log('простое число')
+}else {
+    console.log('не простое число')
+}
+
+
+
