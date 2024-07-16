c7960504d990370bc40c3a0abc22769093f1fda2
ViktorJun <viktorbylash@gmail.com>
2024-07-16 18:16:33 +0300

    homework_5.3

--- homework_5/main.js
+++ homework_5/main.js
@@ -6,11 +6,27 @@
 // }
 
 // homework_5.2
-let dollar = 26
-let uah = prompt(`Введите суму в долларах (1 доллар = ${dollar} грн)`)
-if (!isNaN(Number(uah)) && uah !== null && uah.trim() !== '') {
-    let convert = dollar * uah
-    alert(`Результат конвертации: ${convert}`)
-} else {
-    alert(`Нужно ввести число`)
+// let dollar = 26
+// let uah = prompt(`Введите суму в долларах (1 доллар = ${dollar} грн)`)
+// if (!isNaN(Number(uah)) && uah !== null && uah.trim() !== '') {
+//     let convert = dollar * uah
+//     alert(`Результат конвертации: ${convert}`)
+// } else {
+//     alert(`Нужно ввести число`)
+// }
+
+// homework_5.3
+let number = prompt('Введите число')
+let flag = true
+let i = 0
+while (flag) {
+    i = ++i
+    let sqrt = i * i
+    if (Number.isInteger(sqrt)){
+        if (sqrt <= number && i <= 100) {
+            console.log(sqrt)
+        } else {
+            flag = false
+        }
+    }
 }
