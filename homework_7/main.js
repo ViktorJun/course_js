17e8d0f190bec7c20f64a96b2650ef056d0b08bc
ViktorJun <viktorbylash@gmail.com>
2024-07-19 00:44:38 +0300

    homework_7.1

---
+++ homework_7/main.js
@@ -0,0 +1,14 @@
+// // homework_7.1
+// Написати функцію, яка приймає 1 параметр. Та скадае значення з тим, що передали перший раз і т. д. Все це із замиканнями, наприклад:
+// console.log(sum(4)); // 4
+// console.log(sum(6)); // 10
+// console.log(sum(10)); // 20
+// console.log(sum(7)); // 27
+let result = 0
+const getSum = (num) => {
+    return result = result + num
+}
+console.log(getSum(4)); // 4
+console.log(getSum(6)); // 10
+console.log(getSum(10)); // 20
+console.log(getSum(7)); // 27
