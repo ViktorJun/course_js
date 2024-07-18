91d80828e667a497c47a951cc606814a5a8ef5df
ViktorJun <viktorbylash@gmail.com>
2024-07-18 15:47:37 +0300

    homework_6.3

--- homework_6/main.js
+++ homework_6/main.js
@@ -18,16 +18,34 @@
 
 // homework_6.2
 // Дано масив з елементами різних типів. Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву.
-let inputArray = ['string', 2, true, 5, NaN, 11, null, '400']
-function average(array) {
-    let number = 0
-    let quantity = 0
+// let inputArray = ['string', 2, true, 5, NaN, 11, null, '400']
+// function average(array) {
+//     let number = 0
+//     let quantity = 0
+//     for (let i = 0; i < array.length; i++) {
+//         if (typeof array[i] === 'number' && !isNaN(array[i])) {
+//             number += array[i]
+//             quantity += 1
+//         }
+//     }
+//     return number / quantity
+// }
+// console.log(average(inputArray))
+
+// homework_6.3
+// Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
+// Наприклад:
+// const array = [1, 3, 4, 6, 2, 5, 7];
+// removeElement(array,4);
+// console.log(array); // Результат: [1, 3, 6, 2, 5, 7]
+const inputArray = [1, 3, 4, 6, 2, 5, 7];
+function removeElement(array, element) {
     for (let i = 0; i < array.length; i++) {
-        if (typeof array[i] === 'number' && !isNaN(array[i])) {
-            number += array[i]
-            quantity += 1
+        if (array[i] === element) {
+            array.splice(i, 1);
+            i--
         }
     }
-    return number / quantity
+    return array
 }
-console.log(average(inputArray))
+console.log(removeElement(inputArray, 4))
