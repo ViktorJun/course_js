2e049488f21672cf4c6dc775b7be5532bb38e1b8
ViktorJun <viktorbylash@gmail.com>
2024-07-22 18:57:19 +0300

    homework_7.3

--- homework_7/main.js
+++ homework_7/main.js
@@ -16,12 +16,66 @@
 // homework_7.2
 // Створити функцію для розрахунку добутку двох чисел, що викликається так: name(5)(2).
 // Функція повинна повертати результат (у середині функції не має бути консоль лога!)
-function addingNum(firstNum) {
-    return function (secondNum) {
-        return firstNum + secondNum
+// function addingNum(firstNum) {
+//     return function (secondNum) {
+//         return firstNum + secondNum
+//     }
+// }
+// console.log(addingNum(5)(2))
+
+// homework_7.3
+// Цикл на кожній ітерації пропонує через prompt ввести число більше 100 (але максимум 10 ітерацій циклу).
+// Якщо відвідувач ввів число менше ста – попросити ввести ще раз, і таке інше.
+// Якщо користувач вводить більше ста, текст або цикл закінчує всі ітерації, то функція виводить в консоль останній введення користувача і завершує функцію.
+
+// first option
+(function () {
+    let flag = true
+    let counter = 1
+    const counterMax = 10
+    const minNum = 100
+    let inputNum = prompt('Введите число больше ста')
+    while (flag) {
+        inputNum = Number(inputNum)
+        if (inputNum > minNum && counter !== counterMax) {
+            counter++
+            inputNum = prompt('Введите число больше ста')
+        }else if (inputNum < minNum) {
+            inputNum = prompt('Введите число БОЛЬШЕ ста')
+        }else if (isNaN(inputNum)) {
+            inputNum = prompt('Это не число, введите число больше ста')
+        }else{
+            console.log(`Ваше последнее число: ${inputNum}`)
+            flag = false
+        }
     }
-}
-console.log(addingNum(5)(2))
+})();
+// second option
+(function () {
+    let flag = true
+    let counter = 1
+    const counterMax = 10
+    const minNum = 100
+    let inputNum = prompt('Введите число больше ста')
+    while (flag) {
+        let lastInputNum = inputNum
+        inputNum = Number(inputNum)
+        if (counter === counterMax){
+            alert(`Ваш последний ввод: ${lastInputNum}`)
+            flag = false
+        }else if (inputNum > minNum) {
+            alert(`Ваше число: ${inputNum}`)
+            flag = false
+        }else if (inputNum < minNum) {
+            counter++
+            inputNum = prompt('Введите число БОЛЬШЕ ста')
+        }else {
+            counter++
+            inputNum = prompt('Это не число, введите число больше ста')
+        }
+    }
+})();
+
 
 
 
